491cc6efb96244803be654094217d9ca85ca816a
Farhan696969 <ffebrianto83@gmail.com>
2023-12-11 23:22:02 +0700

    membuat riwayat harga

--- resources/views/admin/coba.blade.php
+++ resources/views/admin/coba.blade.php
@@ -27,15 +27,15 @@
 
 <body>
 
-  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 ">
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 ">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/dashboardadmin">Jayalaksana</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
+    <div class="bg-dark w-100"> </div>
     <div class="navbar-nav">
       <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
+      <a class="nav-link px-3 btn-primary" href="#">Sign out</a>
       </div>
     </div>
   </header>
@@ -47,7 +47,7 @@
         <div class="position-sticky pt-3">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/dashboardadmin">
+              <a class="nav-link" href="/dashboardadmin">
                 <span data-feather="home"></span>
                 Dashboard
               </a>
@@ -72,7 +72,7 @@
             </li>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/harga">
+          <a class="nav-link active" aria-current="page" href="/coba">
             <span data-feather="users"></span>
             Update Pergantian Harga
           </a>
@@ -85,179 +85,39 @@
         </div>
       </nav>
 
-
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="container mt-4">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Telur Tersisa</h5>
-                  <input type="number" class="form-control" id="harga" placeholder="" disabled>
-                  <p class="tekan-untuk-mengganti-harga-swE">
-                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalOne">Tambah Stok Telur</button>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title harga-telur-perkg-dmz">Harga Telur Per KG</h5>
-                  <input type="text" class="form-control" id="namabayar" required disabled>
-                  <p class="tekan-untuk-mengganti-harga-swE">
-                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalTwo">Ganti Harga Per KG</button>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Modal One -->
-        <div class="modal fade" id="modalOne" tabindex="-1" aria-labelledby="modalOneLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="modalOneLabel">Tambah Stok Telur</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <form action="#" onsubmit="submitForm1(event)">
-                  <div class="form-group">
-                    <label for="hargaTelur">Stok Telur (per KG)</label>
-                    <input type="number" class="form-control" id="hargaTelur" placeholder="Contoh: 2000">
-                  </div>
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Modal Two -->
-<div class="modal fade" data-bs-backdrop="static" id="modalTwo" tabindex="-1" aria-labelledby="modalTwoLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalTwoLabel">Ganti Harga Lainnya</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form action="#" onsubmit="submitForm(event)">
-          <div class="form-group">
-            <label for="hargaLainnya">Harga </label>
-            <input type="number" class="form-control" id="hargaLainnya" placeholder="Contoh: 50000">
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-<!-- Bootstrap JS and Popper.js -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-<script>
-  function submitForm1(event) {
-    // Prevent the default form submission
-    event.preventDefault();
-
-    // Get the value from the input
-    var hargaLainnyaValue = document.getElementById('hargaTelur').value;
-
-    // Set the value to the 'namabayar' input
-    document.getElementById('harga').value = hargaLainnyaValue;
-
-    // Close the modal
-    $('#modalOne').modal('hide');
-  }
-  function submitForm(event) {
-    // Prevent the default form submission
-    event.preventDefault();
-
-    // Get the value from the input
-    var hargaLainnyaValue = document.getElementById('hargaLainnya').value;
-
-    // Set the value to the 'namabayar' input
-    document.getElementById('namabayar').value = hargaLainnyaValue;
-
-    // Close the modal
-    $('#modalTwo').modal('hide');
-  }
-</script>
+        
 
 
 
 
 
 
-        <h2>Pesanan</h2>
+        <h2>Riwayat Pergantian Harga</h2>
         <div class="table-responsive ">
           <table class="table table-striped table-sm">
             <thead>
               <tr>
-                <th scope="col">Nama</th>
+                
                 <th scope="col">Tanggal</th>
                 <th scope="col">Harga</th>
-                <th scope="col">Status</th>
-                <th scope="col">Edit Status</th>
-                <th scope="col">No.tlp</th>
-                <th scope="col">Alamat</th>
+                
 
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>Messi</td>
+                
                 <td>01-01-2023</td>
                 <td>12.222.122</td>
-                <td>Proses</td>
-                <td>
-                  <div class="dropdown">
-                    <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Edit
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Proses</a></li>
-                      <li><a class="dropdown-item" href="#">Batal</a></li>
-                      <li><a class="dropdown-item" href="#">Terkirim</a></li>
-                      <li><a class="dropdown-item" href="#">BarangMasuk</a></li>
-                    </ul>
-                    </ul>
-                  </div>
-                </td>
-                <td>0818229933</td>
-                <td>Jl.Indramayu Desa,krowok Rt2/Rw4 Petokan:dibelakang penjual baso</td>
+
 
               </tr>
               <tr>
-                <td>Messi</td>
+               
                 <td>01-01-2023</td>
                 <td>12.222.122</td>
-                <td>Proses</td>
-                <td>
-                  <div class="dropdown">
-                    <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Edit
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Proses</a></li>
-                      <li><a class="dropdown-item" href="#">Batal</a></li>
-                      <li><a class="dropdown-item" href="#">Terkirim</a></li>
-                      <li><a class="dropdown-item" href="#">BarangMasuk</a></li>
-                    </ul>
-                    </ul>
-                  </div>
-                </td>
-                <td>0818229933</td>
-                <td>Jl.Indramayu Desa,krowok Rt2/Rw4 Petokan:dibelakang penjual baso</td>
+                
 
               </tr>
 
--- resources/views/admin/update-penggantian-harga.php
+++ resources/views/admin/update-penggantian-harga.php
@@ -27,19 +27,18 @@
 
 <body>
 
-  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 ">
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 ">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/dashboardadmin">Jayalaksana</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
+    <div class="bg-dark w-100"> </div>
     <div class="navbar-nav">
       <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
+      <a class="nav-link px-3 btn-primary" href="#">Sign out</a>
       </div>
     </div>
   </header>
-
   <div class="container-fluid">
 
     <div class="row">
