b09099f2d8595d55c5b48f2777b760995746aebc
dika2005 <nugrahaandhika57@gmail.com>
2023-12-31 01:41:42 +0700

    dika

--- database/migrations/2023_12_27_145301_create_users_table.php
+++ database/migrations/2023_12_27_145301_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->enum('role', ['user', 'admin' , 'suplier'])->default('/user');
+            $table->enum('role', ['user', 'admin' , 'suplier'])->default('user');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
